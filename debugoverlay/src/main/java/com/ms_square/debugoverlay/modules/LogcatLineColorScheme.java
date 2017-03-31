813826b36d39e38783c309b79a02f35e9471953d
Manabu-GT <manabu1984+github@gmail.com>
2017-03-30 22:46:58 -0600

    feat: update default logcat color scheme

--- debugoverlay/src/main/java/com/ms_square/debugoverlay/modules/LogcatLineColorScheme.java
+++ debugoverlay/src/main/java/com/ms_square/debugoverlay/modules/LogcatLineColorScheme.java
@@ -9,18 +9,7 @@ public interface LogcatLineColorScheme {
     @ColorInt
     int getTextColor(LogcatLine.Priority priority, @NonNull String tag);
 
-    /**
-     * Ref...https://material.io/guidelines/style/color.html#color-color-palette
-     */
     LogcatLineColorScheme DEFAULT_COLOR_SCHEME = new LogcatLineColorScheme() {
-
-        private final int MATERIAL_BLUE = Color.parseColor("#1E88E5");
-
-        private final int MATERIAL_GREEN = Color.parseColor("#43A047");
-
-        private final int MATERIAL_YELLOW = Color.parseColor("#FDD835");
-
-        private final int MATERIAL_RED = Color.parseColor("#E53935");
 
         @Override
         public int getTextColor(LogcatLine.Priority priority, String tag) {
@@ -29,22 +18,21 @@ public interface LogcatLineColorScheme {
                     return Color.BLACK;
                 }
                 case DEBUG: {
-                    return MATERIAL_BLUE;
+                    return Color.BLUE;
                 }
                 case INFO: {
-                    return MATERIAL_GREEN;
+                    return Color.GREEN;
                 }
                 case WARNING: {
-                    return MATERIAL_YELLOW;
+                    return Color.YELLOW;
                 }
                 case ERROR:
                 case FATAL:
-                case ASSERT:
-                case SILENT: {
-                    return MATERIAL_RED;
+                case ASSERT: {
+                    return Color.RED;
                 }
                 default: {
-                    return Color.BLACK;
+                    return Color.WHITE;
                 }
             }
         }
